aaef25accda3fd82ec5bc0a5942e3303306eadc7
plof27 <olaf223224@gmail.com>
2019-04-03 14:47:14 -0400

    express boilerplate

---
+++ server.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const express = require("express");
+var cors = require('cors');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require('path');
+const Data = require("./data");
+
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+// this is our MongoDB database
+const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// middleware
+app.use(express.static(path.join(__dirname, "client", "build"))); // for serving up the clientside code
+app.use("/api", router); // api endpoint
+app.use(bodyParser.urlencoded({ extended: false })); // logging stuff
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// this is our get method
+// this method fetches all available data in our database
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// this is our update method
+// this method overwrites existing data in our database
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Data.findOneAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// this is our delete method
+// this method removes existing data in our database
+router.delete("/deleteData", (req, res) => {
+  const { id } = req.body;
+  Data.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// this is our create methid
+// this method adds new data in our database
+router.post("/putData", (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: "INVALID INPUTS"
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// catch all route for sending client code (SHOULD BE THE LAST ROUTE IN THE FILE)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+// launch our backend into a port
+app.listen(8000, () => console.log(`LISTENING ON PORT ${8000}`));
