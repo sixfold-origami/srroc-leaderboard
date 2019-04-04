0ae175fdb8ca4f7c5bd4a0e999618a9af99973bd
plof27 <olaf223224@gmail.com>
2019-04-04 18:01:08 -0400

    remove commented code

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const logger = require("morgan");
 var cors = require('cors');
-// const Data = require("./data");
+
 const userRoute = require('./routes/user');
 const racerRoute = require('./routes/racer');
 
 require("dotenv").config();
 
 const app = express();
-// const router = express.Router();
 const port = process.env.PORT || 5000;
 const dbRoute = process.env.MONGODB_URI || "";
 
@@ -34,61 +33,10 @@ app.use(bodyParser.urlencoded({ extended: false })); // body parsing
 app.use(bodyParser.json());
 app.use(logger("dev")); // mogan logging i guess? i never use this T.T
 app.use(express.static(path.join(__dirname, "client", "build"))); // for serving up the clientside code
-// app.use("/api", router); // api endpoint
 
 // api routes
 app.use('/api/user', userRoute);
 app.use('/api/racer', racerRoute);
-
-// // this is our get method
-// // this method fetches all available data in our database
-// router.get("/getData", (req, res) => {
-//   Data.find((err, data) => {
-//     if (err) return res.json({ success: false, error: err });
-//     return res.json({ success: true, data: data });
-//   });
-// });
-//
-// // this is our update method
-// // this method overwrites existing data in our database
-// router.post("/updateData", (req, res) => {
-//   const { id, update } = req.body;
-//   Data.findOneAndUpdate(id, update, err => {
-//     if (err) return res.json({ success: false, error: err });
-//     return res.json({ success: true });
-//   });
-// });
-//
-// // this is our delete method
-// // this method removes existing data in our database
-// router.delete("/deleteData", (req, res) => {
-//   const { id } = req.body;
-//   Data.findOneAndDelete(id, err => {
-//     if (err) return res.send(err);
-//     return res.json({ success: true });
-//   });
-// });
-//
-// // this is our create methid
-// // this method adds new data in our database
-// router.post("/putData", (req, res) => {
-//   let data = new Data();
-//
-//   const { id, message } = req.body;
-//
-//   if ((!id && id !== 0) || !message) {
-//     return res.json({
-//       success: false,
-//       error: "INVALID INPUTS"
-//     });
-//   }
-//   data.message = message;
-//   data.id = id;
-//   data.save(err => {
-//     if (err) return res.json({ success: false, error: err });
-//     return res.json({ success: true });
-//   });
-// });
 
 // catch all route for sending client code (SHOULD BE THE LAST ROUTE IN THE FILE)
 app.get("*", (req, res) => {
